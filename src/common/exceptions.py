32d2dc42484d6f60b6aa316837868c95e08171cd
cn47 <integral.rabbit@gmail.com>
2025-12-07 21:47:30 +0900

    fix: Restructure exception hierarchy by introducing DomainError and updating related exceptions

--- src/common/exceptions.py
+++ src/common/exceptions.py
@@ -2,22 +2,25 @@ class ApplicationError(Exception):
     """アプリケーションのエラー"""
 
 
-class UnsupportedFileTypeError(ApplicationError):
+class DomainError(Exception):
+    """ドメインのエラー"""
+
+
+class UnsupportedFileTypeError(DomainError):
     """サポートされていないファイルタイプのエラー"""
 
 
-class TaggingError(ApplicationError):
+class TaggingError(DomainError):
     """タグ付けのエラー"""
+
+
+class DuplicateImageError(DomainError):
+    """重複画像のエラー"""
+
+
+class ImageNotFoundError(DomainError):
+    """画像が見つからないエラー"""
 
 
 class InfrastructureError(Exception):
     """インフラストラクチャーのエラー"""
-
-
-class DuplicateImageError(InfrastructureError):
-    """重複画像のエラー"""
-
-
-class ImageNotFoundError(InfrastructureError):
-    """画像が見つからないエラー"""
-
